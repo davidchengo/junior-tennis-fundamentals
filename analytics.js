abb46d5a30c99fd639c0eda6554056f8dc2210a8
David <2781568+davidchengo@users.noreply.github.com>
2026-08-17 23:54:20 -0400

    Add reusable Google Analytics tracking script

---
+++ analytics.js
@@ -0,0 +1,10 @@
+// Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-SKW160VX2J');
+
+const gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-SKW160VX2J';
+document.head.appendChild(gaScript);
