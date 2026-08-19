581d45a24f236fe628e14dfc8776e131a3570733
David Chen <davidchen@MacBook-Pro.local>
2026-08-18 20:04:54 -0400

    Move Rally School analytics to dedicated GA4 property

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-SKW160VX2J');
+gtag('config', 'G-1TX2KE2DGG');
 
 const gaScript = document.createElement('script');
 gaScript.async = true;
-gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-SKW160VX2J';
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-1TX2KE2DGG';
 document.head.appendChild(gaScript);
 
 // ── Nav scroll effect ──
